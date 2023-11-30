27ffeea1591f3d188b3066c7a673066cff5afaa6
lelewei66 <lelewei667@gmail.com>
2023-11-30 13:37:24 -0500

    update main

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import React from 'react';
-import Header from './components/header/Header'
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+
+
 function App() {
   return (
     <>
       <Header />
+      <main className='main'>
+      <Home />
+      </main>
+     
     </>
   );
 }
